dbf779928ab055741e0d4e66e3a80387bba0ecd1
JosefGold <myemail@gmail.com>
2016-07-16 01:35:20 +0300

    Startoff face rec service

---
+++ Vision/SmartMirror.FaceRecognition/SmartMirror.FaceRecognition.Service/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartMirror.FaceRecognition.Service
+{
+    public class Class1
+    {
+        // SignalR + Angular http://henriquat.re/server-integration/signalr/integrateWithSignalRHubs.html
+        //Video Streaming: http://weblogs.asp.net/ricardoperes/video-streaming-with-asp-net-signalr-and-html5
+    }
+}
